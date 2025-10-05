78c7759c9778614bfc1bf327079006c1c2bc8e16
Lucas <lucasalvesgnecco@gmail.com>
2025-10-05 10:02:28 -0300

    atualizando model & criando collection de static e media

--- contact/admin.py
+++ contact/admin.py
@@ -6,7 +6,7 @@ from contact import models
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'created_date')
     ordering = 'id',
-    # list_filter = 'created_date',
+    # list_filter = 'created_date', # Filtrar a lista com data
     search_fields = 'first_name', 'last_name', 'phone', 'email',
     list_per_page = 10
     list_max_show_all = 200
--- project/settings.py
+++ project/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ( BASE_DIR / 'base_static',)
 
+STATIC_ROOT = BASE_DIR / 'static' # collectstatic
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media' # upload
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
