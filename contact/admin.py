006947b444f237875fc89c59a279023fe7efbeff
Lucas <lucasalvesgnecco@gmail.com>
2025-10-05 12:14:49 -0300

    incrementando a tabela ao index & filtrando na view

--- contact/admin.py
+++ contact/admin.py
@@ -5,12 +5,12 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'created_date', 'category', 'owner', 'show')
-    ordering = 'id',
+    ordering = '-id',
     # list_filter = 'created_date', # Filtrar a lista com data
     search_fields = 'first_name', 'last_name', 'phone', 'email',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'id', 'phone',
 
 @admin.register(models.Category)
