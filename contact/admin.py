9642c844dfb03fe473e41a1f90c9b76e84058b84
Lucas <lucasalvesgnecco@gmail.com>
2025-10-05 11:13:28 -0300

    Script gerador de 1000 contatos

--- contact/admin.py
+++ contact/admin.py
@@ -10,8 +10,8 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = 'first_name', 'last_name', 'phone', 'email',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'phone', 'email',
-    list_display_links = 'id', 'first_name',
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
