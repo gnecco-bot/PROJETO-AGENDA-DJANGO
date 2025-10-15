a69681f1338443991feab2e0a39f9e6472a6b455
Lucas <lucasalvesgnecco@gmail.com>
2025-10-15 20:36:04 -0300

    atualizando view & busca por id

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from contact.models import Contact
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects\
+    contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[:10]
+        .order_by('-id')[10:20]
 
     context = {
         'contacts': contacts,
@@ -14,5 +14,22 @@ def index(request):
     return render(
         request,
         'contact/index.html',
-        context,
-        )
+        context
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
