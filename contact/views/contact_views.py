8e4ff89cafea40063486fef2dd5735b42eb62d23
Lucas <lucasalvesgnecco@gmail.com>
2025-10-15 21:57:23 -0300

    add buscador por fname/lname/phone/email

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 # Create your views here.
@@ -9,6 +10,35 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')    
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) 
+            )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
@@ -18,14 +48,17 @@ def index(request):
     )
 
 
+
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title,   
     }
 
     return render(
