d66c5013773edf1394cf631c1825ac251549001a
Lucas <lucasalvesgnecco@gmail.com>
2025-10-19 11:15:07 -0300

    Adicionando paginacao

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,21 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Q
+
 from contact.models import Contact
 
 # Create your views here.
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[10:20]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -34,9 +40,14 @@ def search(request):
             Q(email__icontains=search_value) 
             )\
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value,
     }
